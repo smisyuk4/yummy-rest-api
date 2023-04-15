5f032a49923d8df47d0eaf91332d395daa276890
Sergiy Misyuk <smisyuk@gmail.com>
2023-04-15 21:48:55 +0300

    write search recipes structure and make query to base

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,25 +1,47 @@
-const {
-    getAllRecipes,
-  } = require('../services/recipesServices');
-  
-  // const { contactValidSchema } = require('../service/schemas/contactValidSchema');
-  // const { ValidationError } = require('../helpers/error');
-  
-  const get = async (req, res) => {
-    const condition = {}
-  
-    const results = await getAllRecipes(condition);
-  
-    res.json({
-      status: 'Success',
-      code: 200,
-      data: {
-        ingretients: results,
-      },
-    });
-  };
-  
-  module.exports = {
-    get,
-  };
-  
+const { getAllRecipes, getRecipes } = require('../services/recipesServices');
+
+// const { contactValidSchema } = require('../service/schemas/contactValidSchema');
+// const { ValidationError } = require('../helpers/error');
+
+const get = async (req, res) => {
+  const condition = {};
+
+  const results = await getAllRecipes(condition);
+
+  res.json({
+    status: 'Success',
+    code: 200,
+    data: {
+      ingretients: results,
+    },
+  });
+};
+
+const search = async (req, res) => {
+  const { title, ingredients, page = 1, limit = 10 } = req.query;
+  console.log(req.query);
+
+  // pagination and filter
+  const skip = (page - 1) * limit;
+
+  const condition = title
+    ? { title: { $regex: title, $options: 'i' } }
+    : { ingredients: { $regex: ingredients, $options: 'i' } };
+  const pagination = { skip, limit };
+
+  console.log(condition, pagination);
+  const results = await getRecipes(condition, pagination);
+
+  res.json({
+    status: 'Success',
+    code: 200,
+    data: {
+      recipes: results,
+    },
+  });
+};
+
+module.exports = {
+  get,
+  search,
+};
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 ## .env
 `PORT=3000`
 `MONGO_URI=mongodb+srv://smisyuk:ser198@sm-dev.mlsnh80.mongodb.net/so-yammy`
+
+## mongo query filters
+[mongodb.com](https://www.mongodb.com/docs/manual/reference/operator/query-comparison/)
