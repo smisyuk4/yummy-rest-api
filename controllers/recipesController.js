16daa2f9198eae0b04760c33b2971f056c092931
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2023-04-18 12:33:50 +0300

    create fnctions pop

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -3,9 +3,10 @@ const {
   getRecipes,
   getRecipesById,
   getRecipesMain,
-} = require('../services/recipesServices');
-const { Recipes } = require('../services/schemas/recipes');
-const { getAllIngredients } = require('../services/ingredientsServices')
+  getPopularRecipes,
+} = require("../services/recipesServices");
+const { Recipes } = require("../services/schemas/recipes");
+const { getAllIngredients } = require("../services/ingredientsServices");
 
 // const { contactValidSchema } = require('../service/schemas/contactValidSchema');
 // const { ValidationError } = require('../helpers/error');
@@ -16,7 +17,7 @@ const get = async (req, res) => {
   const results = await getAllRecipes(condition);
 
   res.json({
-    status: 'Success',
+    status: "Success",
     code: 200,
     data: {
       ingretients: results,
@@ -27,14 +28,14 @@ const get = async (req, res) => {
 const searchByTitle = async (req, res) => {
   const { title, page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const condition = { title: { $regex: title, $options: 'i' } };
+  const condition = { title: { $regex: title, $options: "i" } };
   const pagination = { skip, limit };
 
   console.log(condition, pagination);
   const results = await getRecipes(condition, pagination);
 
   res.json({
-    status: 'Success',
+    status: "Success",
     code: 200,
     data: {
       currentPage: page,
@@ -47,15 +48,15 @@ const searchByTitle = async (req, res) => {
 const searchByIngredients = async (req, res) => {
   const { ttl, page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const condition = { ttl: { $regex: ttl, $options: 'i' } };
+  const condition = { ttl: { $regex: ttl, $options: "i" } };
   const pagination = { skip, limit };
 
   console.log(condition, pagination);
   const allIngredients = await getAllIngredients(condition, pagination);
 
   const exampleEngredients = [
-    { _id: '640c2dd963a319ea671e3746', ttl: 'Potatoes' },
-    { _id: '640c2dd963a319ea671e3768', ttl: 'Small Potatoes' },
+    { _id: "640c2dd963a319ea671e3746", ttl: "Potatoes" },
+    { _id: "640c2dd963a319ea671e3768", ttl: "Small Potatoes" },
   ];
 
   console.log(allIngredients);
@@ -67,7 +68,7 @@ const searchByIngredients = async (req, res) => {
   const recipesByIngredients = await getRecipes(conditionSearch, pagination);
 
   res.json({
-    status: 'Success',
+    status: "Success",
     code: 200,
     data: {
       // currentPage: page,
@@ -86,8 +87,7 @@ const getRecipesByIdController = async (req, res) => {
 const getAllRecipesController = async (req, res, next) => {
   const recipes = await Recipes.find({});
   res.json({
-
-    status: 'success',
+    status: "success",
     code: 200,
     data: {
       result: recipes,
@@ -96,9 +96,8 @@ const getAllRecipesController = async (req, res, next) => {
 };
 
 const popularRecipesController = async (req, res) => {
-  const favoriteLength = await getAllRecipes.keys(favorite).length;
-  // const mostPopular = ?
-  // result
+  const favourite = req.params.favourite;
+  const result = await getPopularRecipes(favourite);
 
   res.json({
     status: "Success",
--- services/recipesServices.js
+++ services/recipesServices.js
@@ -1,11 +1,11 @@
-const { Recipes } = require('./schemas/recipes');
+const { Recipes } = require("./schemas/recipes");
 
 const getAllRecipes = async (condition, pagination) => {
-  return Recipes.find(condition, '', pagination);
+  return Recipes.find(condition, "", pagination);
 };
 
 const getRecipes = async (condition, pagination) => {
-  const recipes = Recipes.find(condition, '', pagination);
+  const recipes = Recipes.find(condition, "", pagination);
   if (!recipes) {
     // throw new WrongParametersError(`Not found contact id: ${contactId}`);
   }
@@ -13,18 +13,22 @@ const getRecipes = async (condition, pagination) => {
   return recipes;
 };
 
+const getPopularRecipes = async (condition, pagination) => {
+  const recipes = Recipes.find(condition, "", pagination);
+  return recipes.sort((a, b) => b.favourite - a.favourite);
+};
+
 const getRecipesById = async (id) => {
   // const result = await Recipes.findOne({ _id: id });
-
   // if (!result) {
   //   throw new AppError(404, `Recipes with id ${id} not found`);
   // }
   // return result;
 };
 
-
 module.exports = {
   getAllRecipes,
   getRecipes,
   getRecipesById,
+  getPopularRecipes,
 };
