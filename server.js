bcd1d3ee44ffa564fc4fe67ec9d35fb49dce7e78
Oksana Polyakova <ddreamer22011986@gmail.com>
2023-04-18 18:28:28 +0300

    swagger json file auth

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 require("dotenv").config();
 
 const {userRouter} = require("./routes/userRoutes");
@@ -14,12 +16,15 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
+const options = {
+	explorer: true
+  };
 
 app.use("/user", userRouter);
 app.use("/recipes", recipesRouter);
 app.use("/ingredients", ingredientsRouter);
 // app.use('/popular-recipe')
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 app.use((_, res, __) => {
 	res.status(404).json({
 		status: "error",
@@ -29,6 +34,10 @@ app.use((_, res, __) => {
 	});
 });
 
+app.use((err, req, res, next) => {
+	const {status = 500, message = "Server error"} = err
+	res.status(status).json({message})
+  })
 // app.use(errorMiddleware)
 
 
