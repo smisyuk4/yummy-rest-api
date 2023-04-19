8a0ef6505165222f50123c3c24683177c10f4fba
IrynaRybka <irarybka@gmail.com>
2023-04-19 14:56:37 +0300

    get-point and create routs

--- routes/ingredientsRoutes.js
+++ routes/ingredientsRoutes.js
@@ -5,6 +5,7 @@ const { get, getAllRecipesByIngredientController } = require('../controllers/ing
 const { asyncWrapper } = require('../helpers/asyncWrapper')
 // const { authMiddleware } =  require('../middlewares/authMiddleware')
 // const { upload } = require('../middlewares/uploadMiddleware')
+const { postIngredientShoppingList, getShoppingList, deleteItemShoppingList } = require('../controllers/shoppingListController');
 
 // ingredients
 // ingredients/list
@@ -14,5 +15,15 @@ router.get('/', asyncWrapper(get));
 router.get('/global', asyncWrapper(getAllRecipesByIngredientController))
 
 router.get('/list', asyncWrapper(get));
+
+// ==== SHOPPING LIST ======
+// add ingredient to shopping-list
+router.post('/shopping-list', postIngredientShoppingList);
+
+// delete ingredient from shopping list
+router.delete('/shopping-list/:id', deleteItemShoppingList)
+
+// get user`s shopping list
+router.get('/shopping-list/:userId', getShoppingList);
 
 module.exports = { ingredientsRouter: router };
