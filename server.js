180edec21fde6bc63e36784c285170d68ded0022
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-04-19 20:00:01 +0300

    modified:   server.js 	modified:   swagger.json

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+require("dotenv").config();
 
-const { ownRecipesRouter } = require("./routes/ownRecipesRoutes");
-const { protectPath } = require('./middlewares/authMiddleware');
-const { userRouter } = require('./routes/userRoutes');
-const { recipesRouter } = require('./routes/recipesRoutes');
-const { ingredientsRouter } = require('./routes/ingredientsRoutes');
-const { errorMiddleware } = require('./middlewares/errorMiddleware');
-
+const {ownRecipesRouter} = require("./routes/ownRecipesRoutes");
+const {protectPath} = require("./middlewares/authMiddleware");
+const {userRouter} = require("./routes/userRoutes");
+const {recipesRouter} = require("./routes/recipesRoutes");
+const {ingredientsRouter} = require("./routes/ingredientsRoutes");
+const {errorMiddleware} = require("./middlewares/errorMiddleware");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/user', userRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/user", userRouter);
 app.use(protectPath);
-app.use('/recipes', recipesRouter);
-app.use('/ingredients', ingredientsRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/recipes", recipesRouter);
+app.use("/ingredients", ingredientsRouter);
 
 app.use((_, res, __) => {
-  res.status(404).json({
-    status: 'error',
-    code: 404,
-    message: 'Not found',
-    data: 'Not found',
-  });
+	res.status(404).json({
+		status: "error",
+		code: 404,
+		message: "Not found",
+		data: "Not found",
+	});
 });
 
 app.use(errorMiddleware);
@@ -43,14 +42,14 @@ mongoose.Promise = global.Promise;
 const connection = mongoose.connect(uriDb);
 
 connection
-  .then(() => {
-    console.log(`Connection to DB done!`);
+	.then(() => {
+		console.log(`Connection to DB done!`);
 
-    app.listen(PORT, function () {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    });
-  })
+		app.listen(PORT, function () {
+			console.log(`Server running. Use our API on port: ${PORT}`);
+		});
+	})
 
-  .catch(err =>
-    console.log(`Server not running. Error message: ${err.message}`)
-  );
+	.catch(err =>
+		console.log(`Server not running. Error message: ${err.message}`)
+	);
