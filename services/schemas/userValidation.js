e1377bf6ebc299f8aac78b1b6a828f4e9997e4b5
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-04-22 17:19:14 +0300

    fix bug from QA

--- services/schemas/userValidation.js
+++ services/schemas/userValidation.js
@@ -1,23 +1,30 @@
 const Joi = require("joi");
-const passwordRegex = /^[a-zA-Z\d]{6,}$/;
+const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
 const nameRegex = /^[a-zA-Zа-яА-ЯґҐєЄїЇіІ\d]{6,}$/;
+const emailRegex = /^[\w\.]+@([\w]+\.)+[\w]{2,4}$/;
 const makeRequired = x => x.required();
 
 const userValidation = (data, strict = []) => {
 	let schema = Joi.object({
 		name: Joi.string().trim().min(1).max(16).regex(nameRegex).messages({
-			"string.pattern.base": `Name may contain letters, numbers, apostrophe, dash and spaces. For example Jacob Mercer.`,
+			"string.pattern.base": `Name may contain letters and numbers. For example Jacob Mercer.`,
 		}),
-		email: Joi.string().email({
-			minDomainSegments: 2,
-		}),
+		email: Joi.string()
+			.email({
+				minDomainSegments: 2,
+			})
+			.trim()
+			.regex(emailRegex)
+			.messages({
+				"string.pattern.base": `Email may only latin letters, numbers and _ @ . symbols.`,
+			}),
 		password: Joi.string()
 			.trim()
 			.min(6)
 			.max(16)
 			.regex(passwordRegex)
 			.messages({
-				"string.pattern.base": `Password can contain upper and lower case letters, numbers and one of special characters @!#$%& and minimum 8 characters.`,
+				"string.pattern.base": `Password must contain upper and lower case letters, numbers and minimum 6 characters.`,
 			}),
 		avatarURL: Joi.string().uri(),
 	});
