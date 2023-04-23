e016db08f710e616b16a5f0abd72bf1aaacf78b5
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-04-23 21:57:56 +0300

    Email fix

--- services/schemas/userValidation.js
+++ services/schemas/userValidation.js
@@ -1,7 +1,8 @@
 const Joi = require("joi");
 const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
 const nameRegex = /^[a-zA-Zа-яА-ЯґҐєЄїЇіІ\d\s]{6,}$/;
-const emailRegex = /^[\w\.]{1,64}@([\w]+\.)+[\w]{2,4}$/;
+const emailRegex =
+	/^[\w!#$%^&*\-=/{}[\]_|`~?\\+][\w!#$%^&*\+\-?=/{}[\]_|`~\\.]{1,62}[\w!#$%^&*\-=/{}+[\]_|`~?\\]@([\w]+\.){1,20}[\w]{1,4}$/;
 const makeRequired = x => x.required();
 
 const userValidation = (data, strict = []) => {
@@ -10,7 +11,7 @@ const userValidation = (data, strict = []) => {
 			"string.pattern.base": `Name can include numbers and letters (Latin, Cyrillic), the minimum number of characters in the field is 1 (inclusive), the maximum is 16 (inclusive)`,
 		}),
 		email: Joi.string().trim().regex(emailRegex).messages({
-			"string.pattern.base": `The local part of the mail can contain capital and small Latin letters (A-Z, a-z), numbers (from 0 to 9), but no more than 64 characters`,
+			"string.pattern.base": `The local part of the mail can contain capital and small Latin letters (A-Z, a-z), numbers (from 0 to 9) and special symbols !#$%^&*_-=*/?+ but no more than 64 characters`,
 		}),
 		password: Joi.string()
 			.trim()
