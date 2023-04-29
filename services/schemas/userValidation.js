96ca6299ca357a026f68c5757acc25eda8a94939
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-04-29 13:16:54 +0300

    Fix user name

--- services/schemas/userValidation.js
+++ services/schemas/userValidation.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
-const nameRegex = /^[a-zA-Zа-яА-ЯґҐєЄїЇіІ\d\s]{6,}$/;
+const nameRegex = /^[a-zA-Zа-яА-ЯґҐєЄїЇіІ\d\s]{1,}$/;
 const emailRegex =
 	/^[\w!#$%^&*\-=/{}[\]_|`~?\\+][\w!#$%^&*\+\-?=/{}[\]_|`~\\.]{1,62}[\w!#$%^&*\-=/{}+[\]_|`~?\\]@([\w]+\.){1,20}[\w]{1,4}$/;
 const makeRequired = x => x.required();
